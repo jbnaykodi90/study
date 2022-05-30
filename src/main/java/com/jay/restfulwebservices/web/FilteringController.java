ad346e87df0ef2d375ae3877e0b2df1cf0887bc7
jbnaykodi90 <jbnaykodi90@gmail.com>
2022-05-30 23:32:00 +0530

    Code added for filtering response

---
+++ src/main/java/com/jay/restfulwebservices/web/FilteringController.java
@@ -0,0 +1,23 @@
+package com.jay.restfulwebservices.web;
+
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.jay.restfulwebservices.model.SomeBean;
+
+@RestController
+public class FilteringController {
+	@GetMapping("/filtering-bean")
+	public MappingJacksonValue getSomeBean() {
+		SomeBean someBean = new SomeBean("val1", "val2", "val3");
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+		mapping.setFilters(filters);
+		return mapping;
+	}
+}
